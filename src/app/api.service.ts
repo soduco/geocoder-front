ba02d7abe39f53349236ffe4654b253036a668c5
theohuard <theo.huard@ensg.eu>
2022-04-07 11:54:24 +0200

    little change for running application

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { catchError, Observable, throwError } from 'rxjs';
+
+import { Observable, throwError } from 'rxjs';
 
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { retry, catchError } from 'rxjs/operators';
